c5da6a2052fb6c087dfc242727bbf505cbcf474c
KhangVuVan21a <khang21a@gmail.com>
2022-05-20 09:46:27 +0700

    OOP finish2

--- src/dao/IDao.java
+++ src/dao/IDao.java
@@ -3,13 +3,39 @@ package dao;
 import java.util.ArrayList;
 
 public interface IDao {
+	/**
+	 * add Object into data
+	 * @param row
+	 * @return
+	 */
 	public int insert(Object row);
 
+	/**
+	 * update object in data
+	 * @param row
+	 * @return
+	 */
 	public int update(Object row);
 
+	/**
+	 *  delete object
+	 * @param row
+	 * @return
+	 */
 	public boolean delete(Object row);
 
+	/**
+	 * get all data by name
+	 * @param name
+	 * @return
+	 */
 	public ArrayList findAll(String name);
 
+	/**
+	 * find data by id
+	 * @param name
+	 * @param id
+	 * @return
+	 */
 	public Object findById(String name, int id);
 }
